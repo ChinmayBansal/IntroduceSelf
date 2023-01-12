8dd179dd7d97f918e36d384d6fed2b080af59235
Chinmay Bansal <chinmaybansalsc@outlook.com>
2023-01-12 14:40:20 -0500

    Introduction

--- PreWork/ViewController.swift
+++ PreWork/ViewController.swift
@@ -9,6 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var FirstNameTextField: UITextField!
+    
+    @IBOutlet weak var LastNameTextField: UITextField!
+    
+    @IBOutlet weak var SchoolTextField: UITextField!
+    
+    @IBOutlet weak var AcademicYear: UISegmentedControl!
+    
+    
+    @IBOutlet weak var PetsCounterLabel: UILabel!
+    
+    @IBOutlet weak var PetsStepper: UIStepper!
+    @IBAction func IncrementPets(_ sender: UIStepper) {
+        PetsCounterLabel.text = "\(Int(sender.value))"
+    }
+    
+    @IBAction func introduceSelf(_ sender: UIButton) {
+        let year = AcademicYear.titleForSegment(at: AcademicYear.selectedSegmentIndex)
+        let intro = "My first name is \(FirstNameTextField.text!) \(LastNameTextField.text!) and I attend \(SchoolTextField.text!). I am currently in my \(year!) year and I own \(PetsCounterLabel.text!) pets. It is \(MorePetsSwitch.isOn) more pets"
+        let alert = UIAlertController(title: "My Introduction", message: intro, preferredStyle: .alert)
+        let action = UIAlertAction(title: "Nice to meet you", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+    }
+    @IBOutlet weak var MorePetsSwitch: UISwitch!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
